1b3ffc7b2d488c17eaf54fc79894a0996d595443
Gabriel Torres Rivera <gtorresr@purdue.edu>
2019-06-05 11:42:14 -0400

    added test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a=float(3)
+b=float(4)
+
+c= (a**2 + b**2)**(1/2)
+
+print(c)
+#sys.path.append('/Users/julie/Downloads/Test.wav')
+#
+#path = '/Users/julie/Downloads/Test.wav'
+#days_file = open(path,'r')
+#days = days_file.read()
